f2b8f6a4d80f067e11c32b1be5faa4d617ccb69b
Bermet <bermetk.6789@gmail.com>
2021-06-29 16:05:27 +0600

    add task 1

--- exam/src/com/company/Main.java
+++ exam/src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import java.util.HashSet;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(5);
+        int[] arr = new int[100];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i + 1;
+        }
+        HashSet<Integer> eventArr = new HashSet<>();
+        //мы знаем что в HashSet нет порядкаа
+        //но я гуглила не нашла способ по порядку
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0 && arr[i] % 5 == 0) {
+                    eventArr.add(arr[i]);
+            }
+        }//но почему-то если я делаю так то все добавилось в возрастающем порядке
+        System.out.println(eventArr);
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0 && arr[i] % 5 != 0) {
+                eventArr.add(arr[i]);
+            }
+        }
+        System.out.println(eventArr);
     }
 }
