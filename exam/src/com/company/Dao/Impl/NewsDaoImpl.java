9ac59ba22837c46c872438f271e6fb2faa2756a7
Bermet <bermetk.6789@gmail.com>
2021-07-27 00:21:52 +0600

    add task 2

--- exam/src/com/company/Dao/Impl/NewsDaoImpl.java
+++ exam/src/com/company/Dao/Impl/NewsDaoImpl.java
@@ -4,11 +4,36 @@ import com.company.Dao.NewsDao;
 import com.company.Model.NewsModel;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import static com.company.Dao.BaseDao.connect;
 
 public class NewsDaoImpl implements NewsDao {
+
+    public ArrayList<NewsModel> getAll() throws SQLException {
+        ArrayList <NewsModel> newsAll = null;
+        try (
+                Connection connection = connect();
+                PreparedStatement statement = connection.prepareStatement("SELECT * FROM news");
+                ResultSet resultSet = statement.executeQuery()) {
+            newsAll = new ArrayList<>();
+
+            while (resultSet.next()) {
+                NewsModel news = new NewsModel();
+                news.setId(resultSet.getInt("id"));
+                news.setHeadline(resultSet.getString("headline"));
+                news.setText(resultSet.getString("text_news"));
+                news.setTimeOfPublication(resultSet.getTime("time_of_publication"));
+                newsAll.add(news);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return newsAll;
+    }
+
     public void insert(NewsModel newsModel) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите заголовок новости: ");
@@ -20,13 +45,14 @@ public class NewsDaoImpl implements NewsDao {
     @Override
     public void createNews() {
         try (Connection connection = connect();
-             PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO news(header, text_news, time_of_publication)" +
+             PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO news(headline, text_news, time_of_publication)" +
                      "values(?, ?, now())")) {
             NewsModel newsModel = new NewsModel();
             insert(newsModel);
             preparedStatement.setString(1, newsModel.getHeadline());
             preparedStatement.setString(2, newsModel.getText());
             preparedStatement.executeUpdate();
+            System.out.println("Новость добавлена!");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
@@ -38,7 +64,7 @@ public class NewsDaoImpl implements NewsDao {
              Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery("SELECT * FROM news WHERE id = " + newsModel.getId())) {
             if (resultSet.next()) {
-                System.out.println(resultSet.getInt("header"));
+                System.out.println(resultSet.getString("headline"));
                 System.out.println(resultSet.getString("text_news"));
             }
         } catch (Exception e) {
@@ -49,9 +75,10 @@ public class NewsDaoImpl implements NewsDao {
     @Override
     public void deleteNews(NewsModel newsModel) {
         try (Connection connection = connect();
-             PreparedStatement preparedStatement = connection.prepareStatement("DELETE * FROM news WHERE id = ?")) {
+             PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM news WHERE id = ?")) {
             preparedStatement.setInt(1, newsModel.getId());
             preparedStatement.executeUpdate();
+            System.out.println("Новость удалена!");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
@@ -62,13 +89,15 @@ public class NewsDaoImpl implements NewsDao {
         Scanner sc = new Scanner(System.in);
         try (Connection connection = connect();
              PreparedStatement preparedStatement = connection.prepareStatement("UPDATE news SET headline = ?, text_news = ? WHERE id = ?")) {
-            System.out.println("Введите новый заголовок:");
+            System.out.println("Введите новый заголовок: ");
             newsModel.setHeadline(sc.nextLine());
             preparedStatement.setString(1, newsModel.getHeadline());
-            System.out.println("Введите новый текст:");
+            System.out.println("Введите новый текст: ");
             newsModel.setText(sc.nextLine());
-            preparedStatement.setString(3, newsModel.getText());
+            preparedStatement.setString(2, newsModel.getText());
+            preparedStatement.setInt(3, newsModel.getId());
             preparedStatement.executeUpdate();
+            System.out.println("Новость изменена!");
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
--- exam/src/com/company/Main.java
+++ exam/src/com/company/Main.java
@@ -4,12 +4,39 @@ import com.company.Dao.Impl.NewsDaoImpl;
 import com.company.Dao.NewsDao;
 import com.company.Model.NewsModel;
 
-public class Main {
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        NewsModel news = new NewsModel();
-        NewsDao newsDao = new NewsDaoImpl();
-        newsDao.createNews();
-        news.
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) throws SQLException {
+        NewsDaoImpl newsDao = new NewsDaoImpl();
+        ArrayList<NewsModel> newsModels = new ArrayList<>();
+
+        while (true){
+            System.out.println("1.Создать новость\n2.Отобразить новость\n3.Изменить новость по id\n4.Удалить новость по id\nВведите пункт меню: \n");
+            int choice = sc.nextInt();
+            switch (choice){
+
+                case 1: newsDao.createNews();break;
+                case 2: newsDao.showNews(choose(newsModels));break;
+                case 3: newsDao.updateNews(choose(newsModels));break;
+                case 4: newsDao.deleteNews(choose(newsModels));break;
+            }
+        }
+
+
+
+    }
+
+    static NewsModel choose(List<NewsModel> newsModels) throws SQLException {
+        NewsDaoImpl newsDao = new NewsDaoImpl();
+        newsModels.addAll(newsDao.getAll());
+        System.out.println("\nВведите номер новости: ");
+        int id = sc.nextInt();
+        return newsModels.get(id - 1);
     }
 }
